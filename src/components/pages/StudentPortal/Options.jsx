42a59788a12db437bcc604223f4837222c1016dd
drjerry-sys <64374109+drjerry-sys@users.noreply.github.com>
2021-10-22 11:08:12 +0100

    remove onClick from options links

--- src/components/pages/StudentPortal/Options.jsx
+++ src/components/pages/StudentPortal/Options.jsx
@@ -2,48 +2,48 @@ import React from 'react';
 import './StudentPortal.scss';
 import { Link } from 'react-router-dom';
 
-const Options = ({ optionSelected, exam }) => {
+const Options = ({ option, exam }) => {
 
     const paper = exam === "exam" ? 'Exam' : "Test";
 
     return ( 
         <div className="portalSubOptions">
             {
-                optionSelected === 'res' ? (
+                option === 'results' ? (
                     <>
                         <Link className="subLink">All Results</Link >
                         <Link className="subLink">Recent Term</Link >
                         <Link className="subLink">Recent Session</Link >
                     </>
-                ) : optionSelected === 'cbt' ? (
+                ) : option === 'cbt' ? (
                     <>
                         <Link className="subLink">{paper} Time Table</Link>
                         <Link className="subLink">Take {paper}</Link>
                         <Link className="subLink">Recent Session</Link>
                     </>
-                ) : optionSelected === 'pay' ? (
+                ) : option === 'payments' ? (
                     <>
                         <Link className="subLink">Make Payments</Link>
                         <Link className="subLink">Payment History</Link>
                     </>
-                ) : optionSelected === 'sub' ? (
+                ) : option === 'subjects' ? (
                     <>
                         <Link className="subLink">List of Books</Link>
                         <Link className="subLink">List of Subjects</Link>
                     </>
-                ) : optionSelected === 'attend' ? (
+                ) : option === 'attendance' ? (
                     <>
                         <Link className="subLink">Mark Attenndance</Link>
                         <Link className="subLink">Term History</Link>
                         <Link className="subLink">All History</Link>
                     </>
-                ) : optionSelected === 'chat' ? (
+                ) : option === 'chat' ? (
                     <>
                         <Link className="subLink">Chat Room</Link>
                         <Link className="subLink">Discussion Groups</Link>
                         <Link className="subLink">Recent Session</Link>
                     </>
-                ) : optionSelected === 'updt' ? (
+                ) : option === 'updates' ? (
                     <>
                         <Link className="subLink">Notice</Link>
                         <Link className="subLink">Recent Updates</Link>
--- src/components/pages/StudentPortal/StudentPortal.jsx
+++ src/components/pages/StudentPortal/StudentPortal.jsx
@@ -11,28 +11,21 @@ const StudentPortal = () => {
 
     const {option} = useParams();
 
-    const handleSelect = (value) => {
-        setOptionSelected(value);
-    }
-
     return ( 
         <>
             <Navbar portalType="student" />
             <div className="studentPortal">
-                {/* <div className="portalTitle">
-                    <p>Student Portal</p>
-                </div> */}
                 <div className="portalOptions">
                     <div className="buttonsGroup">
-                        <Link to="/student-portal/updates" onClick={() => handleSelect('updt')} className="groupBtn">Updates</Link>
-                        <Link to="/student-portal/results" onClick={() => handleSelect('res')} className="groupBtn">Results</Link>
-                        <Link to="/student-portal/cbt" onClick={() => handleSelect('cbt')} className="groupBtn">Exam/Test</Link>
-                        <Link to="/student-portal/payments" onClick={() => handleSelect('pay')} className="groupBtn">Payments</Link>
-                        <Link to="/student-portal/subjects" onClick={() => handleSelect('sub')} className="groupBtn">Subjects/Books</Link>
-                        <Link to="/student-portal/attendance" onClick={() => handleSelect('attend')} className="groupBtn">Attendance</Link>
-                        <Link to="/student-portal/chat" onClick={() => handleSelect('chat')} className="groupBtn">Chat Room</Link>
+                        <Link to="/student-portal/updates" className="groupBtn">Updates</Link>
+                        <Link to="/student-portal/results" className="groupBtn">Results</Link>
+                        <Link to="/student-portal/cbt" className="groupBtn">Exam/Test</Link>
+                        <Link to="/student-portal/payments" className="groupBtn">Payments</Link>
+                        <Link to="/student-portal/subjects" className="groupBtn">Subjects/Books</Link>
+                        <Link to="/student-portal/attendance" className="groupBtn">Attendance</Link>
+                        <Link to="/student-portal/chat" className="groupBtn">Chat Room</Link>
                     </div>
-                    <Options optionSelected={optionSelected} exam="test" />
+                    <Options option={option} exam="test" />
                     <span className="currentTitle">Third Term 2018/2019</span>
                     { 
                         option === 'results' ? <TableForResults /> : 
